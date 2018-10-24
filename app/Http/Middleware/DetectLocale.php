884922d79ed2800f5bf909622b4beb2436455739
dotSILENT <dot.silentium@gmail.com>
2018-10-24 15:59:38 +0200

    Optimise DetectLocale a little more

--- app/Http/Middleware/DetectLocale.php
+++ app/Http/Middleware/DetectLocale.php
@@ -17,27 +17,23 @@ class DetectLocale
     public function handle(Request $request, Closure $next)
     {
         $locale = config('app.locale'); // if no preferred language is found at all, use the default
+        $supportedLocales = array_fill_keys(config('app.supported_locales'), 1); // optimisation, avoids looping through supported locales array by using a key-value pair
         
         if($request->has('lang'))
         {
             $locale = $request->get('lang');      
-            if(!in_array($locale, config('app.supported_locales')))
+            if(!isset($supportedLocales[$locale]))
                 $locale = config('app.fallback_locale'); // if requested language is not supported, use the fallback locale
         }
         else
         {
-            //$locale = $request->getPreferredLanguage();
             $userLangs = preg_split('/,|;/', $request->server('HTTP_ACCEPT_LANGUAGE')); // accept-language header varies from browser to browser
-            $counter = 0;
             foreach($userLangs as $lang)
             {
-                if(++$counter >= 100) // forged Accept-Language header? Prevent unneccessary looping
-                    break;
                 /**
                  * Find the first user-preferred language that our app supports
-                 * Ignore values starting with q= to prevent unneccessary looping
                  */
-                if(substr($lang,2) != 'q=' && in_array($lang, config('app.supported_locales')))
+                if(isset($supportedLocales[$lang]))
                 {
                     $locale = $lang;
                     break;
@@ -46,7 +42,6 @@ class DetectLocale
         }
 
         app()->setLocale($locale);
-
         return $next($request);
     }
 }
